8a49567e0a8b9e0054044fbe02b4910ed6193564
fazleyhadi <152062616+fazleyhadi@users.noreply.github.com>
2025-10-16 01:06:40 +0600

    Add files via upload

---
+++ Firstlesson/python1.py
@@ -0,0 +1,65 @@
+print('hello world');
+
+# variable_and_data_types
+
+age=30;
+price=9.5;
+name='Hamza';
+x=True;
+y=None;
+
+print(type(age));
+print(type(price));
+print(type(name));
+print(type(x));
+print(type(y));
+
+# Operators
+
+m=10;
+n=5;
+
+# arithmetic_operators(+,-,*,/,**,%)
+
+print('m+n=',(m+n));
+print('m-n=',(m-n));
+print('m*n=',(m*n));
+print('m/n=',(m/n));
+print('m**n=',(m**n));
+print('m%n=',(m%n));
+#relational_operators(==,!=,>,>=,<,<=).---output always comes in boolean values--
+print(m==n);
+print(m!=n);
+print(m>n);
+print(m>=n);
+print(m<n);
+print(m<=n);
+#assignment_operators(=,+=,-=,*=,/=,%=,**=)
+num=5;    #outputs
+ #num+=2; #7
+ #num-=2; #3
+ #num*=2; #10
+ #num**=2; #25
+ #num/=2; #2.5
+ #num%=2; #1
+#  print('num:',num);
+
+#Logical_Operators
+
+a=5;
+b=3;
+print(not False);
+print(not True);
+print(not a>b);
+val1=True;
+val2=True;
+print('and operator:',val1 and val2);
+print('and operator:',(a>b) and val2);
+print('and operator:',(a<b) and val2);
+print('or operator:',val1 or val2);
+print('or operator:',(a>b) or val2);
+print('or operator:',(a<b) or val2);
+
+
+
+
